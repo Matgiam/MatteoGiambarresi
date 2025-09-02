c58215cad136443df511229556485d62373357e1
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-09-02 19:19:40 +0200

    Added the foolish minds section

--- src/components/FoolishMinds.jsx
+++ src/components/FoolishMinds.jsx
@@ -3,7 +3,12 @@ function FoolishMindsText() {
 		<div>
 			<div className="project_text" id="foolishminds">
 				<h1>Foolish Minds</h1>
-				<p>Take a look at all the projects I have <br /> made during the past two years <br /> during my education</p>
+				<p>
+					Take a look at all the projects I have <br /> made during the past two years <br /> during my education
+				</p>
+				<div className="button">
+					<a>Follow us</a>
+				</div>
 			</div>
 		</div>
 	);
