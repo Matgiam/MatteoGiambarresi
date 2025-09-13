f3cc04a8bd5c04d074cc781ab3640a6bff77d45d
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-09-13 23:28:05 +0200

    added classnames for the cardstacker to be manipulated easier for the responsive version

--- src/components/Cardstacker.jsx
+++ src/components/Cardstacker.jsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import move from "lodash-move";
 
+
 const CARD_IMAGES = [
-	"./src/assets/matteo_4.jpeg",
-	"./src/assets/matteo_1.jpg",
-	"./src/assets/matteo_2.png",
-	"./src/assets/matteo_3.jpeg",
-	"./src/assets/matteo_5.jpeg",
+  "./src/assets/matteo_4.jpeg",
+  "./src/assets/matteo_1.jpg",
+  "./src/assets/matteo_2.png",
+  "./src/assets/matteo_3.jpeg",
+  "./src/assets/matteo_5.jpeg",
 ];
 
 const CARD_OFFSET = 10;
@@ -15,63 +16,43 @@ const SCALE_FACTOR = 0.06;
 const ROTATION_FACTOR = 5;
 
 const CardStack = () => {
-	const [cards, setCards] = useState(CARD_IMAGES);
+  const [cards, setCards] = useState(CARD_IMAGES);
 
-	const moveToEnd = (from) => {
-		setCards(move(cards, from, cards.length - 1));
-	};
+  const moveToEnd = (from) => {
+    setCards(move(cards, from, cards.length - 1));
+  };
 
-	return (
-		<div style={wrapperStyle}>
-			<ul style={cardWrapStyle}>
-				{cards.map((image, index) => {
-					const canDrag = index === 0;
-					const offsetX = index * 20;
-					const rotation = index * ROTATION_FACTOR;
+  return (
+    <div className="cardstacker-wrapper">
+      <ul className="cardstacker-cardwrap">
+        {cards.map((image, index) => {
+          const canDrag = index === 0;
+          const offsetX = index * 20;
+          const rotation = index * ROTATION_FACTOR;
 
-					return (
-						<motion.li
-							key={image}
-							style={{
-								...cardStyle,
-								backgroundImage: `url(${image})`,
-								backgroundSize: "cover",
-								cursor: canDrag ? "grab" : "auto",
-							}}
-							animate={{
-								top: index * -CARD_OFFSET,
-								left: offsetX,
-								scale: 1 - index * SCALE_FACTOR,
-								rotate: rotation,
-								zIndex: CARD_IMAGES.length - index,
-							}}
-							drag={canDrag ? "y" : false}
-							dragConstraints={{ top: 0, bottom: 0 }}
-							onDragEnd={() => moveToEnd(index)}
-						/>
-					);
-				})}
-			</ul>
-		</div>
-	);
+          return (
+            <motion.li
+              key={image}
+              className={`cardstacker-card ${canDrag ? "cardstacker-card--draggable" : ""}`}
+              style={{
+                backgroundImage: `url(${image})`,
+              }}
+              animate={{
+                top: index * -CARD_OFFSET,
+                left: offsetX,
+                scale: 1 - index * SCALE_FACTOR,
+                rotate: rotation,
+                zIndex: CARD_IMAGES.length - index,
+              }}
+              drag={canDrag ? "y" : false}
+              dragConstraints={{ top: 0, bottom: 0 }}
+              onDragEnd={() => moveToEnd(index)}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
-const wrapperStyle = {
-	position: "relative",
-};
-
-const cardWrapStyle = {
-	position: "relative",
-};
-
-const cardStyle = {
-	position: "absolute",
-	width: "32vw",
-	height: "80vh",
-	borderRadius: "15px",
-	transformOrigin: "bottom right",
-	display: "none",
-};
-
-/* https://codesandbox.io/p/sandbox/card-stack-framer-motion-e0v68?file=%2Fsrc%2Findex.js%3A25%2C23-42%2C13 */
 export default CardStack;
