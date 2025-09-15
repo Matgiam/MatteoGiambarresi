702c81b34a3b253a2635ea1b83883626ff7aab17
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-09-15 20:17:11 +0200

    ahlalala

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/MatteoGiambarresi/', // matches your GitHub Pages repo name
-})
+}))
